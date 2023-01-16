2cbb098cab34d2cfa3e178e6a102ebcb073940a7
jasmin <aljasmin933@gmail.com>
2023-01-16 11:11:58 +0400

    modification

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 
 import './App.css';
+import Todoslist from './components/Todoslist';
 
-import Todo from './components/todo/Todo';
+import Header from './components/Header';
+import Form from './components/Form';
+import React, { useState } from 'react'
 
-function App() {
- 
+const App = () => {
 
-   
+  const [input,setInput]=useState("");
+  const [todos,setTodos]= useState([]);
+  const [editTodo,setEditTodo]=useState(null);
+
   return (
-    <div className="App">
-      <div className='heading'>
-       <div>
-          <Todo />
-       </div>
-        
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+         
+        </div>
+        <div>
+        <Form 
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+        />
+        </div>
+        <div>
+          <Todoslist todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+        </div>
       </div>
     </div>
-  );
+    
+    
+  )
 }
 
-export default App;
+export default App
+
+
